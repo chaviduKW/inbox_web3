10c8998529a5246edca8667b9787623da9199385
Chavidu <chavidukalpana40@gmail.com>
2023-10-15 23:51:10 +0530

    fetching accounts from ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,29 +5,42 @@ const { Web3 } = require('web3');
 
 const web3 = new Web3(ganache.provider())
 
-class Car {
-    park(){
-        return 'stopped';
-    }
+// class Car {
+//     park(){
+//         return 'stopped';
+//     }
 
-    drive(){
-        return 'vroom';
-    }
-}
+//     drive(){
+//         return 'vroom';
+//     }
+// }
 
-let car;
+// let car;
+
+// beforeEach(()=>{
+//     car = new Car();
+// })
+
+// describe('Car',()=>{
+//     it('can park',()=>{
+//         assert.equal(car.park(),'stopped');
+//     });
+
+//     it('can drive',()=>{
+//         assert.equal(car.drive(),'vroom');
+//     });
+// })
 
 beforeEach(()=>{
-    car = new Car();
+    // Get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts=>{
+            console.log(fetchedAccounts);
+        })
+    // Use one of those accounts to deploy the contract
 })
 
-describe('Car',()=>{
-    it('can park',()=>{
-        assert.equal(car.park(),'stopped');
-    });
-
-    it('can drive',()=>{
-        assert.equal(car.drive(),'vroom');
-    });
+describe('Inbox',()=>{
+    it('deploys a contract',()=>{});
 })
 
